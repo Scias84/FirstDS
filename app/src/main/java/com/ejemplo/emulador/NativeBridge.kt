e95535cfc1194e53b921bea29162f1158124c175
Scias84 <boteo.0884@gmail.com>
2026-08-23 08:44:52 -0600

    Update NativeBridge.kt

--- app/src/main/java/com/ejemplo/emulador/NativeBridge.kt
+++ app/src/main/java/com/ejemplo/emulador/NativeBridge.kt
@@ -4,12 +4,11 @@ import java.nio.ByteBuffer
 
 object NativeBridge {
     init {
-        // Carga la librería nativa compilada por CMake
         System.loadLibrary("emulatorkernel")
     }
 
     external fun nativeInit(systemPath: String): Boolean
-    external fun nativeLoadRom(romPath: String): Boolean
+    external fun nativeLoadRom(romPath: String): String
     external fun nativeRunFrame(keyMask: Int, touchX: Int, touchY: Int, isTouching: Boolean)
     external fun nativeGetTopBuffer(): ByteBuffer?
     external fun nativeGetBottomBuffer(): ByteBuffer?
